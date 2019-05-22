7f4220b3bf48713433d392a119995b4304d2d97f
diete <diete@139.174.105.196>
2019-05-22 10:00:04 +0200

    Initial

---
+++ HelloGitWorld/src/HelloGitWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloGitWorld {
+	public String World1 = "Hello World 1";
+	public String World2 = "Hello World 2";
+	
+	public static void main (String [] args) {
+		System.out.println("");
+	}
+}
